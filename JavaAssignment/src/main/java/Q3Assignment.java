dbc03dd889f95925f4e14b95779c3c059f81495c
Saikiran <59529306+movvasaikiran25@users.noreply.github.com>
2024-04-07 18:21:26 +0530

    Add files via upload

---
+++ JavaAssignment/src/main/java/Q3Assignment.java
@@ -0,0 +1,17 @@
+public class Q3Assignment {
+
+    public static void main(String[] args) {
+
+        int a, b;
+        a = 10;
+        b = 90;
+
+
+        if (a == 10 && b == 70) {
+            System.out.println("Square of a given number " + a + " "+ b + "  =  " + ((a * a) + (b * b) + (2 * a * b)));
+        } else if (a == 10 && b == 90) {
+            System.out.println("Cube of a given number " + a + " "+ b + "  =  " + ((a * a * a) + (b * b * b) + (3 * a * a * b) + (3 * a * b * b)));
+
+        }
+    }
+}
